227e856e9ba2106d9ffa6d4c06b6d9cc9e2f92a6
Simon6024 <simon.houle1454@hotmail.com>
2017-02-10 20:57:07 -0500

    Map-reduce tasks work and give expected sorted output

--- src/WhoToFollow.java
+++ src/WhoToFollow.java
@@ -1,6 +1,13 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.function.Predicate;
 import org.apache.hadoop.conf.Configuration;
@@ -58,61 +65,12 @@ public class WhoToFollow{
                 Integer friend = Integer.parseInt(st.nextToken());
                 friend1.set(friend);
                 //emit all (friend, user) pair
-                context.write(friend1,user);
+                context.write(user,friend1);
             }
         }
     }
     
     public static class ReducerOne extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
-
-        // A private class to describe a recommendation.
-        // A recommendation has a friend id and a number of friends in common.
-        private static class Recommendation {
-
-            // Attributes
-            private int friendId;
-            private int nCommonFriends;
-
-            // Constructor
-            public Recommendation(int friendId) {
-                this.friendId = friendId;
-                // A recommendation must have at least 1 common friend
-                this.nCommonFriends = 1;
-            }
-
-            // Getters
-            public int getFriendId() {
-                return friendId;
-            }
-
-            public int getNCommonFriends() {
-                return nCommonFriends;
-            }
-
-            // Other methods
-            // Increments the number of common friends
-            public void addCommonFriend() {
-                nCommonFriends++;
-            }
-
-            // String representation used in the reduce output            
-            public String toString() {
-                return friendId + "(" + nCommonFriends + ")";
-            }
-
-            // Finds a representation in an array
-            public static Recommendation find(int friendId, ArrayList<Recommendation> recommendations) {
-                for (Recommendation p : recommendations) {
-                    if (p.getFriendId() == friendId) {
-                        return p;
-                    }
-                }
-                // Recommendation was not found!
-                return null;
-            }
-
-        }
-
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
         {
         	//Holds all the values
@@ -147,37 +105,74 @@ public class WhoToFollow{
 
     public static class ReducerTwo extends Reducer<IntWritable, IntWritable, IntWritable, Text> 
     {
+		public void countFollowers(int follower, HashMap<Integer, Integer> allFollowers)
+		{
+			if(follower < 0)
+			{
+				allFollowers.put(follower*-1, -1);
+			}
+			else
+			{
+				if(allFollowers.containsKey(follower))
+				{
+					int number = allFollowers.get(follower);
+					if(number != -1)
+					{
+						allFollowers.put(follower, allFollowers.get(follower) + 1);
+					}
+				}
+				else
+				{
+					allFollowers.put(follower, 1);    				
+				}
+			}
+		}	
+    	
+		public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map )
+		{
+		    List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+		    Collections.sort(list, new Comparator<Map.Entry<K, V>>()
+		    {
+		        @Override
+		        public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+		        {
+		            return ( o1.getValue() ).compareTo( o2.getValue() );
+		        }
+		    } );
+	
+		    Map<K, V> result = new LinkedHashMap<>();
+		    
+		    for (Map.Entry<K, V> entry : list)
+		    {
+		        result.put(entry.getKey(), entry.getValue());
+		    }
+		    return result;
+		}
+	
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
         {
-        	//Holds all the values
-            ArrayList<Integer> users = new ArrayList<Integer>();
-        	
+        	HashMap<Integer, Integer> allFollowers = new HashMap<Integer,Integer>();
+        	ArrayList<Integer> temp = new ArrayList<>();
             while (values.iterator().hasNext()) 
             {
                  int value = values.iterator().next().get();
-                 users.add(value);
-            }           
-            Text emittedValues;
-            IntWritable newKey = new IntWritable();
-            for(Integer u : users)
+                 countFollowers(value,allFollowers);
+                 temp.add(value);
+            }             
+            
+            Map<Integer, Integer> sortedFollowers = sortByValue(allFollowers);
+            
+            StringBuilder sb = new StringBuilder();
+            for(Map.Entry<Integer, Integer> follower : sortedFollowers.entrySet())
             {
-                String curValues = ((Integer)(-1*key.get())).toString();//values that will be emitted (starts with -key)
-            	for (Integer u2 : users)
+            	if(follower.getValue()>0 && follower.getKey() != key.get())
             	{
-            		if(!u.equals(u2))
-            		{
-            			curValues += " " + u2.toString();
-            		}
+            		sb.append(" "+follower.getKey()+"("+follower.getValue()+")");
             	}
-            	
-            	newKey.set(u);
-            	emittedValues = new Text(curValues);
-            	context.write(newKey,emittedValues);
-            	//Emitting all permutations of the values ex: for key = 3 values = [1 2] 
-            	//It emits Key = 1 Values = [-3 2] and Key = 2 Values = [-3 1]
             }
+            Text output = new Text(sb.toString());
+            context.write(key, output);
         }   
-
     }
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException 
     {
